068a60b56c7cf6d58bab8922d1b8058273945d48
learneffectivejava <learneffectivejava@gmail.com>
2020-03-15 09:41:13 +0530

    java.lang.Object class does not have any abstract method.

---
+++ src/main/java/com/effectivejava/corejava/CoreJava1.java
@@ -0,0 +1,20 @@
+package com.effectivejava.corejava;
+
+/**
+ * A sample java class which shows that it extends java.lang.Object class. It
+ * proves that java.lang.Object does not contain any abstract method as we do
+ * not implement any method.
+ * 
+ * @author learneffectivejava
+ *
+ */
+public class CoreJava1 {
+
+	public static void main(String[] args) {
+		CoreJava1 coreJava1 = new CoreJava1();
+		// There is no method in this class, but we can call methods inherited from
+		// java.lang.Object class
+		System.out.println(coreJava1.hashCode());
+		System.out.println(coreJava1.toString());
+	}
+}
