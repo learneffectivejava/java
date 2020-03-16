6215547bb098212acdc2a5e21e6b8748e97fde02
learneffectivejava <learneffectivejava@gmail.com>
2020-03-16 07:07:11 +0530

    * Immutable vs. Mutable class in Java

---
+++ src/main/java/com/effectivejava/corejava/CoreJava2.java
@@ -0,0 +1,22 @@
+package com.effectivejava.corejava;
+
+/**
+ * Immutable vs. Mutable class in Java
+ * 
+ * @author learneffectivejava
+ *
+ */
+public class CoreJava2 {
+
+	public static void main(final String[] args) {
+		String str1 = "Hello";
+		String str2 = str1;
+		str1 += "World";
+		System.out.println(str1 + " " + str2 + " " + (str1 == str2));
+
+		StringBuffer sb1 = new StringBuffer("Hello");
+		StringBuffer sb2 = sb1;
+		sb1.append("World");
+		System.out.println(sb1 + " " + sb2 + " " + (sb1 == sb2));
+	}
+}
